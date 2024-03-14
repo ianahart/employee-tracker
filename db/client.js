59d7e0822976a7d8a9da542b2f832804b9809655
Ian Hart <ianhart7790@gmail.com>
2024-03-14 16:12:53 -0400

    feat: setup database with dummy test call to retrieve all employees - will remove later

--- db/client.js
+++ db/client.js
@@ -1,10 +1,29 @@
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
+require('dotenv').config();
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: process.env.DB_PASSWORD,
-  database: 'employee_db',
-});
+let connection; // module-level variable to store the connection
 
-module.exports = connection;
+const createMySQLConnection = async () => {
+  try {
+    if (!connection) {
+      connection = await mysql.createConnection({
+        host: 'localhost',
+        port: '3306',
+        user: 'root',
+        password: process.env.DB_PASSWORD,
+        database: 'employee_db',
+      });
+    }
+    return connection;
+  } catch (error) {
+    console.error('Error establishing database connection:', error);
+    throw error;
+  }
+};
+
+// export a function to get the database connection
+module.exports = {
+  getConnection: async () => {
+    return await createMySQLConnection();
+  },
+};
